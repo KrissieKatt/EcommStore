fd95712ceab25e9ded7e1378e6054b425e6642ca
Krissie Katt <124374091+KrissieKatt@users.noreply.github.com>
2024-07-19 09:19:06 -0400

    Initial Commit : added assets and page views

---
+++ E-Commerce Swift/ContentView.swift
@@ -0,0 +1,97 @@
+//
+//  ContentView.swift
+//  E-Commerce Swift
+//
+//  Created by Christen Nancy Knight on 7/19/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var currentTab: Tab = .Home
+    
+    init() {
+        UITabBar.appearance().isHidden = true
+    }
+    
+    @Namespace var animation
+    
+    var body: some View {
+        TabView(selection: $currentTab) {
+         
+            Text("Home View")
+            Text("Search View")
+            Text("Notification View")
+            Text("Cart View")
+            Text("Profile View")
+        }
+        .overlay(
+            HStack(spacing : 0){
+                ForEach(Tab.allCases, id: \.rawValue){tab in
+                    TabButton(tab: tab)
+                    
+                    )
+                }
+            }
+    }
+}
+
+struct Contentview_Previews: PreviewProvider{
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+enum Tab: String, CaseIterable{
+    case Home = "house"
+    case Search = "magnifyingglass.circle"
+    case Notification = "bell"
+    case Cart = "bag"
+    case Profile = "person"
+    
+    var Tabname: String {
+        switch self {
+        case .Home:
+            return "home"
+        case .Search:
+            return "magnifyingglass.circle"
+        case .Notification:
+            return "bell"
+        case .Cart:
+            return "bag"
+        case .Profile:
+            return "person"
+        }
+    }
+}
+
+
+extension View{
+    func  getSafeArea() -> UIEdgeInsets{
+        
+        guard let screen = UIApplication.shared.connectedScenes.first as?
+                UIWindowScene else{
+            return .zero
+        }
+        
+        guard let safeArea = screen.windows.first? .safeAreaInsets else{
+            return .zero
+        }
+        
+        return safeArea
+    }
+}
+
+struct MaterialEffect: UIViewRepresentable {
+    var style: UIBlurEffect.Style
+    
+    func makeUIView(context: Context) -> UIVisualEffectView {
+        let view = UIVisualEffectView(effect: UIBlurEffect(stlye:style ))
+        
+        return view
+    }
+    
+    func updateUIView(_ uiView: UIVisualEffectView, context: Context) {
+        
+    }
+}
---
+++ E-Commerce Swift/E_Commerce_SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  E_Commerce_SwiftApp.swift
+//  E-Commerce Swift
+//
+//  Created by Christen Nancy Knight on 7/19/24.
+//
+
+import SwiftUI
+
+@main
+struct E_Commerce_SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
